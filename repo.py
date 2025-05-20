e97978d5d3bf459a2287dd7e1b454fd7b837621d
vadimnet <abakumov.978@gmail.com>
2025-05-20 20:02:24 +0300

    some edits

--- repo.py
+++ repo.py
@@ -90,13 +90,27 @@ class Repository:
             return(characteristics_values)
         except Exception as e:
             raise Exception(e)
-        pass
     
-    def create_characteristics_template(self, characteristics: list, type: int):
+    def create_type(self, characteristics: list, type_name: str):
         try:
-            sql = 'insert into drone_characteristics(name) values'
-            self.cursor.executemany
-            pass
+            id_characteristics = []
+            sql = 'insert into drone_type(name) values(\'%s\')'%type_name
+            self.cursor.execute(sql)
+            self.conn.commit()
+            sql = 'select id_drone_type from drone_type where name = \'%s\''%type_name
+            self.cursor.execute(sql)
+            type = self.cursor.fetchone()
+            for x in characteristics:
+                sql = 'insert into drone_characteristic(name) values (\'%s\')'%x
+                self.cursor.execute(sql)
+                self.conn.commit()
+                sql = 'select id_drone_characteristics from drone_characteristic where name like \'%s\''%x
+                self.cursor.execute(sql)
+                id_characteristics.append(self.cursor.fetchone())
+            for x in id_characteristics:
+                sql = 'insert into drone_characteristics_type(id_drone_type, id_drone_characteristics) values(\'%s\'),(\'%s\')'%type%x
+                self.cursor.execute(sql)
+                self.conn.commit()
         except Exception as e:
             raise Exception(e)
 
@@ -111,3 +125,5 @@ repo = Repository('C:\\Users\\user\\Documents\\data.db')
 print(repo.get_drone_types())
 print(repo.get_characteristics_template(0))
 print(repo.get_characteristics_values(0))
+repo.create_type(['хуета1'], 'хуета2')
+print(print(repo.get_characteristics_template(3)))
