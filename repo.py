9ae35d1344bf07a590bd85fd6a5984f8dc61eed9
vadimnet <abakumov.978@gmail.com>
2025-06-10 16:55:00 +0300

    some changes

--- repo.py
+++ repo.py
@@ -18,29 +18,29 @@ class Repository:
                                     id_drone serial primary key,
                                     name text,
                                     id_drone_type int references drone_type,
-                                    description text)""")
+                                    description text,
+                                    pictures text)""")
 
             self.cursor.execute("""create table if not exists drone_characteristic(
-                                    id_drone_characteristics serial primary key,
+                                    id_drone_characteristic serial primary key,
                                     name text)""")
                                     
-            self.cursor.execute("""create table if not exists drone_characteristics_type(
-                                    id_drone_characteristics_type serial primary key,
+            self.cursor.execute("""create table if not exists drone_characteristic_type(
+                                    id_drone_characteristic_type serial primary key,
                                     id_drone_type int references drone_type,
-                                    id_drone_characteristics int references drone_characteristic,
+                                    id_drone_characteristic int references drone_characteristic,
                                     is_mandatory bool)""")
 
             self.cursor.execute("""create table if not exists drone_characteristic_value(
-                                    id_drone_characteristics_value serial primary key,
+                                    id_drone_characteristic_value serial primary key,
                                     value text,
                                     id_drones int references drone,
                                     id_drone_type int references drone_type,
-                                    id_characteristics int references drone_characteristic)""")
+                                    id_characteristic int references drone_characteristic)""")
             
             self.cursor.execute("""create table if not exists picture(
-                                    id serial primary key,
-                                    blob text,
-                                    id_drone int references drone)""")
+                                    id_picture serial primary key,
+                                    blob text)""")
             
             self.cursor.execute("""insert into drone_type(name) values('Мультироторного типа(стабилизированные)'),
                                 ('Мультироторного типа(FPV)'),
@@ -62,53 +62,53 @@ class Repository:
         except Exception as e:
             raise Exception(e)
         
-    def get_characteristics_template(self, type: int) -> list[str]:
+    def get_characteristic_template(self, type: int) -> list[str]:
         try:
-            characteristics_template = []
+            characteristic_template = []
             sql = """select d_c.name from drone_characteristic d_c
-                                join drone_characteristics_type d_c_t on d_c_t.id_drone_characteristics = d_c.id_drone_characteristics 
+                                join drone_characteristic_type d_c_t on d_c_t.id_drone_characteristic = d_c.id_drone_characteristic 
                                 join drone_type d_t on d_c_t.id_drone_type = d_t.id_drone_type
                                 where d_t.id_drone_type = %s""" %type
             self.cursor.execute(sql)
             rows = self.cursor.fetchall()
             for name in rows:
-                 characteristics_template.append(name)
-            return characteristics_template
+                 characteristic_template.append(name)
+            return characteristic_template
         except Exception as e:
             raise Exception(e)
 
-    def get_characteristics_values(self, drone_id: int) -> list[str]:
+    def get_characteristic_values(self, drone_id: int) -> list[str]:
         try:
-            characteristics_values = []
+            characteristic_values = []
             sql = """select dcv.value from drone_characteristic_value dcv
                         join drone d on d.id_drone = dcv.id_drones
                         where d.id_drone = %s"""%drone_id
             self.cursor.execute(sql)
             rows = self.cursor.fetchall()
             for value in rows:
-                characteristics_values.append(value)
-            return(characteristics_values)
+                characteristic_values.append(value)
+            return(characteristic_values)
         except Exception as e:
             raise Exception(e)
     
-    def create_type(self, characteristics: list, type_name: str):
+    def create_type(self, characteristic: list, type_name: str):
         try:
-            id_characteristics = []
+            id_characteristic = []
             sql = 'insert into drone_type(name) values(\'%s\')'%type_name
             self.cursor.execute(sql)
             self.conn.commit()
             sql = 'select id_drone_type from drone_type where name = \'%s\''%type_name
             self.cursor.execute(sql)
             type = self.cursor.fetchone()
-            for x in characteristics:
+            for x in characteristic:
                 sql = 'insert into drone_characteristic(name) values (\'%s\')'%x
                 self.cursor.execute(sql)
                 self.conn.commit()
-                sql = 'select id_drone_characteristics from drone_characteristic where name like \'%s\''%x
+                sql = 'select id_drone_characteristic from drone_characteristic where name like \'%s\''%x
                 self.cursor.execute(sql)
-                id_characteristics.append(self.cursor.fetchone())
-            for x in id_characteristics:
-                sql = 'insert into drone_characteristics_type(id_drone_type, id_drone_characteristics) values(\'%s\'),(\'%s\')'%type%x
+                id_characteristic.append(self.cursor.fetchone())
+            for x in id_characteristic:
+                sql = 'insert into drone_characteristic_type(id_drone_type, id_drone_characteristic) values(\'%s\'),(\'%s\')'%type%x
                 self.cursor.execute(sql)
                 self.conn.commit()
         except Exception as e:
@@ -123,7 +123,7 @@ class Repository:
 #----------------------------------------------------------------
 repo = Repository('C:\\Users\\user\\Documents\\data.db')
 print(repo.get_drone_types())
-print(repo.get_characteristics_template(0))
-print(repo.get_characteristics_values(0))
+print(repo.get_characteristic_template(0))
+print(repo.get_characteristic_values(0))
 repo.create_type(['хуета1'], 'хуета2')
-print(print(repo.get_characteristics_template(3)))
+print(print(repo.get_characteristic_template(3)))
